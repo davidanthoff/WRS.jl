6ff00720f4069cafd62d6c8bca7d9bcba3b08812
David Anthoff <anthoff@berkeley.edu>
2018-05-08 17:13:15 -0700

    Add Harrell-Davis estimator

--- src/WRS.jl
+++ src/WRS.jl
@@ -1,5 +1,28 @@
+__precompile__()
 module WRS
 
-# package code goes here
+using Distributions
+
+export hd
+
+# This implements (3.16) from (2017) p. 71-72
+function hd(X, q=0.5, issorted=false)
+    n = length(X)
+
+    a = (n+1)*q
+    b = (n+1)*(1-q)
+
+    β = Beta(a, b)
+
+    sorted_X = issorted ? X : sort(X)
+    
+    θ = 0.
+    for i=1:n
+        w = cdf(β, i/n) - cdf(β, (i-1)/n)
+        θ += w*sorted_X[i]
+    end
+
+    return θ
+end
 
 end # module
